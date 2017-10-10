0b1d3978ac03a37708941db640a6d1e229e9a042
LoScienzo <32672927+LoScienzo@users.noreply.github.com>
2017-10-10 15:25:34 +0200

    Find End

--- ConsoleApplication3/Block.h
+++ ConsoleApplication3/Block.h
@@ -5,6 +5,7 @@ class Block
 public:
 	Block();
 	~Block();
+	&char FindEnd(&char);
 
 private:
 
@@ -17,3 +18,10 @@ Block::Block()
 Block::~Block()
 {
 }
+
+&char Block::FindEnd(&char a)
+ {
+	 int i=0;
+	 while(std::find(Op::operators[0],Op::operators[4],a[i+1]))
+	 {}	 
+ }
