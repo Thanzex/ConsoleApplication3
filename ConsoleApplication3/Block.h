a7309123f8cf219c44f93d75c2bf347adbced05e
Thanzex <b.frenc@hotmail.it>
2017-10-10 15:31:12 +0200

    Fix pointer

--- ConsoleApplication3/Block.h
+++ ConsoleApplication3/Block.h
@@ -5,7 +5,7 @@ class Block
 public:
 	Block();
 	~Block();
-	&char FindEnd(&char);
+	char& FindEnd(char&);
 
 private:
 
@@ -19,7 +19,7 @@ Block::~Block()
 {
 }
 
-&char Block::FindEnd(&char a)
+char& Block::FindEnd(char& a)
  {
 	 int i=0;
 	 while(std::find(Op::operators[0],Op::operators[4],a[i+1]))
--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -22,7 +22,6 @@ int main()
 
 	for (auto& c : input)
 	{
-
 	}
 	
     
